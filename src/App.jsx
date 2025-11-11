33b277d692edae22aab16794bcb5760607482586
Lara Pereira <larabernspereira@gmail.com>
2025-11-11 15:08:44 -0300

    feat: add redirect from root path to dashboard in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Auth/Login/Login";
 import SignUp from "./pages/Auth/SignUp/SignUp";
 import Dashboard from "./pages/Dashboard/Dashboard";
@@ -14,6 +14,7 @@ const App = () => {
       <Alert />
 
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
